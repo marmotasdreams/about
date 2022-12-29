4b31c065725e9ae00063f692ffd4aeb28e792fff
Karen Palacio <karen.palacio.1994@gmail.com>
2022-12-29 20:50:24 -0300

    ahora es bilingue. ahora tiene textos mas en serio. ahora tiene estilos botones y redes

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let arrowButton = document.getElementById("arrow")
 let circles = document.getElementById("circles")
 let ball = document.getElementById("ball")
 let twirl = document.getElementById("twirl");
-let description = document.getElementById("description");
+let description = document.getElementById("shortDescription");
+let moreButton = document.getElementById("mas")
 console.log(arrowButton)
 
 
@@ -16,7 +17,8 @@ function appearText() {
     //description.style.overflowX = "none";
     //description.style.overflowY = "auto";
     description.style.overflow = "auto";
-    description.style.background = "red";
+    description.style.color = "white";
+    description.style.background = "black";
 
 
 }
@@ -34,4 +36,45 @@ function addMultipleEventListener(element, events, handler) {
   events.forEach(e => element.addEventListener(e, handler))
 }
 addMultipleEventListener(arrowButton, ['click','touchend'], scroll)
+
+langState = "english"
+
+function setLang(lang) {
+    langState = lang;
+    console.log("INSETLANG")
+    console.log(lang);
+    console.log(texts[lang])
+    description.innerHTML = texts[lang].shortDescription
+
+}
+//window.onload = setLang(langState)
+
+
+let spanishButton = document.getElementById("esp")
+let englishButton = document.getElementById("eng")
+
+function setLangAndReload(event, lang) {
+    console.log("ENRELOAD");
+    console.log(lang)
+    setLang(lang)
+    var refresh = window.localStorage.getItem('refresh');
+    console.log(refresh);
+    if (refresh===null){
+        window.location.reload();
+        window.localStorage.setItem('refresh', "1");
+    }
+
+}
+
+function showLongerDescription(){
+    console.log("Showing longer asdasd ")
+    console.log(texts[langState].longDescription)
+    let description = document.getElementById("shortDescription");
+    description.innerHTML = texts[langState].longDescription
+        console.log(description)
+}
+spanishButton.addEventListener("click", function (e){setLangAndReload(e,"spanish")})
+englishButton.addEventListener("click", function(e){setLangAndReload(e,"english")} )
+moreButton.addEventListener("click",function(e){showLongerDescription()})
+
 
